e98c8761414e22a1f713f22d15c2b8a6439cd665
johnvchuma <john@smartfoundry.co>
2024-10-20 11:34:42 +0300

    save changes

--- index.js
+++ index.js
@@ -24,7 +24,6 @@ const app = express();
 //Middlewares
 app.use("/files", express.static("files"));
 app.use(cors());
-// app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text({ type: "/" }));
